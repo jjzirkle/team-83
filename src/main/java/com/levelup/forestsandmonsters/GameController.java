8a49f88971f6bf54097c75cb5dfaec4bc9ae9341
treylee <trieveon.lee@gmail.com>
2023-09-21 15:45:36 +0000

    fixed their test cases

--- src/main/java/com/levelup/forestsandmonsters/GameController.java
+++ src/main/java/com/levelup/forestsandmonsters/GameController.java
@@ -10,8 +10,12 @@ public class GameController {
         // TODO: Add other status data
         public String characterName = DEFAULT_CHARACTER_NAME;
         public Point currentPosition = null;
+        public int startX = 0;
+        public int startY = 0;
+        public int endX = 0;
+        public int endY = 0;
         // TODO: Write a failing unit test that will force you to set this to the right number
-        public int moveCount = -100;
+        public int moveCount = 0;
     }
 
     GameStatus status;
@@ -39,6 +43,7 @@ public class GameController {
         // TODO: Implement startGame - Should probably create tiles and put the character
         // on them?
         // TODO: Should also update the game results?
+        status.currentPosition = new Point(status.startX,status.startY);
     }
 
     public GameStatus getStatus() {
@@ -48,20 +53,36 @@ public class GameController {
     public void move(DIRECTION directionToMove) {
         // TODO: Implement move - should call something on another class
         // TODO: Should probably also update the game results
+            System.out.println("HERE!!!!!!!!!!!!!!!!!!!");
+        if (directionToMove.toString() == "NORTH") {
+            System.out.println("ENTERED!!!!!!!!!!!!!!!!!!!");
+            status.currentPosition.y = status.currentPosition.y + 1;
+        }
+        if (directionToMove.toString() == "SOUTH") {
+            status.currentPosition.y = status.currentPosition.y - 1;
+        }
+        if (directionToMove.toString() == "WEST") {
+            status.currentPosition.x = status.currentPosition.x - 1;
+        }
+        if (directionToMove.toString() == "EAST") {
+            status.currentPosition.x = status.currentPosition.x + 1;
+        }
+        status.moveCount += 1;
     }
 
     public void setCharacterPosition(Point coordinates) {
-        // TODO: IMPLEMENT THIS TO SET CHARACTERS CURRENT POSITION -- exists to be testable
+        status.currentPosition = coordinates;
     }
 
     public void setCurrentMoveCount(int moveCount) {
         // TODO: IMPLEMENT THIS TO SET CURRENT MOVE COUNT -- exists to be testable
+            status.moveCount = moveCount;
     }
 
     public int getTotalPositions() {
         // TODO: IMPLEMENT THIS TO GET THE TOTAL POSITIONS FROM THE MAP -- exists to be
         // testable
-        return -10;
+        return 100;
     }
 
 }
